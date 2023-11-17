081e48f9c62e0493c18fd4eef768f41896633fa3
Jsinco <jonahmarksinclair@gmail.com>
2023-11-17 17:02:36 -0500

    DB stuff

--- src/main/java/dev/jsinco/hoarder/sql/Database.java
+++ src/main/java/dev/jsinco/hoarder/sql/Database.java
@@ -1,16 +1,19 @@
 package dev.jsinco.hoarder.sql;
 
+import dev.jsinco.hoarder.Hoarder;
 import dev.jsinco.hoarder.objects.TreasureItem;
 import org.bukkit.inventory.ItemStack;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Database {
 
+    private final Hoarder plugin;
 
-    public Database() {
-
+    public Database(Hoarder plugin) {
+        this.plugin = plugin;
     }
 
 
@@ -33,10 +36,36 @@ public abstract class Database {
             PreparedStatement statement = getConnection().prepareStatement("SELECT * FROM treasure_items;");
             ResultSet resultSet = statement.executeQuery();
 
-            return new TreasureItem(identifier, resultSet.getInt("weight"), (ItemStack) resultSet.getObject("itemStack"));
+            List<TreasureItem> treasureItems = new ArrayList<>();
+
+            for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
+                String columnName = resultSet.getMetaData().getColumnName(i);
+                ResultSet rs = getConnection().prepareStatement("SELECT * FROM treasure_items WHERE " + columnName + ";").executeQuery();
+                treasureItems.add(new TreasureItem(rs.getString("identifier"), rs.getInt("weight"), (ItemStack) rs.getObject("itemStack")));
+            }
+
+            return treasureItems;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public long getHoarderEndTime() {
+        try {
+            PreparedStatement statement = getConnection().prepareStatement("SELECT * FROM simple_data;");
+            ResultSet resultSet = statement.executeQuery();
+
+            return resultSet.getInt("hoarder_endtime");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public void initializeTables() {
+        try {
+
         }
     }
 }
--- src/main/java/dev/jsinco/hoarder/sql/MySQL.java
+++ src/main/java/dev/jsinco/hoarder/sql/MySQL.java
@@ -1,10 +1,26 @@
 package dev.jsinco.hoarder.sql;
 
+import dev.jsinco.hoarder.Hoarder;
+
 import java.sql.Connection;
 
 public class MySQL extends Database {
+
+    private final String address;
+    private final String username;
+    private final String password;
+
+    public MySQL(Hoarder plugin) {
+        super(plugin);
+        this.address = "jdbc:mysql://" + plugin.getConfig().getString("storage.address");
+        this.username = plugin.getConfig().getString("storage.username");
+        this.password = plugin.getConfig().getString("storage.password");
+    }
+
     @Override
     public Connection getConnection() {
         return null;
     }
+
+
 }
